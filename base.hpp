870b9dcefe9da1d37866446248b6c319cb091403
Ferenc Simon <ferenc.simon@student.tugraz.at>
2026-06-21 04:40:48 +0200

    version 0.1

--- base.hpp
+++ base.hpp
@@ -2,33 +2,72 @@
 #define BASE_HPP
 
 #include <iostream>
-#include <vector>
 #include <string>
-#include <unordered_map>
+#include <fstream>
+#include <format>
 
 using std::string;
-using std::vector;
 using std::cout;
 using std::endl;
 using std::cin;
-using std::getline;
-using std::unordered_map;
+using std::format;
 
-enum class Errors
-{
-  MEMORY,
-  INVALID_PATH,
-  UNEXPECTED,
-};
-
-class base
+class Base
 {
   public:
-    const static unordered_map<Errors, string> messages = {
-      {Errors::MEMORY, "[ERROR] Memory Allocation Failed"},
-      {Errors::INVALID_PATH, "[ERROR] The path is invalid"},
-      {Errors::UNEXPECTED, "[ERROR] An unexpected error just occured"},
-    };
+    inline static const string WELCOME_LINE = "------------------------------------";
+    inline static const string WELCOME_MSSG = "Welcome to the cpp/hpp file creator!";
+
+    inline static const string PROMPT = "Please input the name of the file you want to create.\n" 
+    "(Use QUIT to end the program)";
+
+    inline static const string INPUT_WAIT = " > ";
+
+    inline static const string INVALID_STRING = "[ERROR] Invalid string given!";
+
+    static bool checkSize(string &input)
+    {
+      if(input.size() <= 0)
+      {
+        cout << endl;
+        cout << Base::INVALID_STRING << endl;
+        cout << endl;
+        return true;
+      }
+
+      return false;
+    }
+
+    static void FirstBigRestSmall(string &input)
+    {
+      for(int i = 0; i < input.size(); i++)
+      {
+        if(i == 0)
+        {
+          input.at(i) = std::toupper(input.at(i));
+        }
+        else
+        {
+          input.at(i) = std::tolower(input.at(i));
+        }
+      }
+    }
+
+    static void AllBig(string &input)
+    {
+      for(int i = 0; i < input.size(); i++)
+      {
+        input.at(i) = std::toupper(input.at(i));
+      }
+    }
+
+    static void AllSmall(string &input)
+    {
+      for(int i = 0; i < input.size(); i++)
+      {
+        input.at(i) = std::tolower(input.at(i));
+      }
+    }
 };
 
-#endif //BASE_HPP
+#endif // BASE_HPP
