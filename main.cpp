22291eff89067b7a975e0b4030acee0124377655
Ferenc Simon <ferenc.simon@student.tugraz.at>
2026-06-23 16:17:42 +0200

    added description

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 //----------------------------------------------------------------------------------------------------------------------
 /// This is the first version of cpp-hpp-file-creator it is very basic but it works :)
+/// This version supports absolute paths if the user types in more than one slash at the beginning when defining
+/// the path where they want their files made
 //----------------------------------------------------------------------------------------------------------------------
 
 #include "base.hpp"
